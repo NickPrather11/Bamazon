a0eed24b83c2e7713bd5a836a90be4aef6eb41b1
Flying Serpent <nickprather11@gmail.com>
2019-07-31 10:04:53 -0400

    comments added for function outlines

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,10 +11,18 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\r\n");
+  // console.log("connected as id " + connection.threadId + "\r\n");
   start();
 });
 
 function start() {
-  // "home page" function that gives you a choice to post or bid
+  // "home page" function that lists items available for sale, asks customer for ID# of the item they'd like to purchase,
+  // followed by a prompt for the quantity they'd like to purchase.
+  // If the entered quantity is available, update the database with the subtracted quantity of that item
+  // and display the cost to the customer.
+  // If the entered quantity is not available, display "Insufficient Quantity!"
+}
+
+function purchase() {
+  // This function updates the database, subtracting the quantity purchased from the item specified.
 }
